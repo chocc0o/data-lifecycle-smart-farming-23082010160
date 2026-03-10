9a786924375f8178dff29325e43005720ed50a95
Khonsa Abidah <khonsaabidah@gmail.com>
2026-03-10 19:31:41 +0700

    requirements.txt

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os
 import warnings
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
